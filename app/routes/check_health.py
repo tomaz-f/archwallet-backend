7784bc8dd1cf36e4abd2d19de1938733090d338f
Felipe Tomaz <tomazalvesfelipe@gmail.com>
2023-04-12 10:12:07 -0400

    fix: url health-check

--- app/routes/check_health.py
+++ app/routes/check_health.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 router = APIRouter(prefix='/server', tags=['Server'])
 
 
-@router.get("/healt-check")
+@router.get("/health-check")
 def healt_check():
     return {"status": "The server is up and running flawless"}
